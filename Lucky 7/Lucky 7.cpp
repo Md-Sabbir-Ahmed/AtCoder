c9a6079395cd193e142a3ec405166e867d077529
Sabbir <sabbir106706@gmail.com>
2020-05-09 17:40:41 +0600

    p5 using cpp

---
+++ Lucky 7/Lucky 7.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string N;
+    cin>>N;
+
+  if(N[0]=='7'|| N[1]=='7'||N[2]=='7')
+    {
+        cout<<"Yes"<<"\n";
+    }
+    else{
+        cout<<"No"<<"\n";
+    }
+
+    return 0;
+}
