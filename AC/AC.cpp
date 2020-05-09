476d35b6fc2323247ef96616fb09a5c345179886
Sabbir <sabbir106706@gmail.com>
2020-05-09 15:20:30 +0600

    p1 using cpp

---
+++ AC/AC.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string S;
+    cin>>S;
+    if(S=="ABC")
+    {
+        cout<<"ARC"<<endl;
+    }
+    else if(S=="ARC")
+    {
+        cout<<"ABC"<<endl;
+    }
+}
