444e34b8c6dc72418b5c29f35d1b8aec10751cf4
Sabbir <sabbir106706@gmail.com>
2020-05-10 00:41:40 +0600

    p9 using cpp

---
+++ Children and Candies (ABC Edit)/Children and Candies (ABC Edit).cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int N,sum=0;
+    cin>>N;
+    for(int i=0;i<=N;i++)
+    {
+        sum=sum+i;
+    }
+    cout<<sum<<"\n";
+
+
+    return 0;
+}
