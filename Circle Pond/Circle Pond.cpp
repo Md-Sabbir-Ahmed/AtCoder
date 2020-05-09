55661ef9076596800bd60863a4f2370cfe85973e
Sabbir <sabbir106706@gmail.com>
2020-05-09 17:08:09 +0600

    p4 using cpp

---
+++ Circle Pond/Circle Pond.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int R;
+    cin>>R;
+    cout<<2*M_PI*R;
+
+    return 0;
+}
+
