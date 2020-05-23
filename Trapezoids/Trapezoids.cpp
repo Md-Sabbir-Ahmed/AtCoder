faceae5c69171804d294bf50ee7bc6179642b571
Sabbir <sabbir106706@gmail.com>
2020-05-24 00:41:38 +0600

    p12 using cpp

---
+++ Trapezoids/Trapezoids.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    float a,b,h,area=0;
+    cin>>a;
+    cin>>b;
+    cin>>h;
+    area=(a+b)*h/2;
+    cout<<area;
+    return 0;
+}
+
