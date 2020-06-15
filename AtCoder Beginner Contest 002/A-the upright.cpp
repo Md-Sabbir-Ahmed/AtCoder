b29466d8604a0c771b3b2795809b4235405abcbc
Sabbir <sabbir106706@gmail.com>
2020-06-16 01:52:31 +0600

    abc002-A

---
+++ AtCoder Beginner Contest 002/A-the upright.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x,a;
+    cin>>x>>a;
+    if(x>a)
+    {
+        cout<<x<<"\n";
+    }
+    else{
+        cout<<a<<"\n";
+    }
+    return 0;
+}
+
+
