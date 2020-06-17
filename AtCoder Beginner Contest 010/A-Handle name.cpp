d3a7bd3a487f156d0c7d1ba2f716b7deeeb19488
Sabbir <sabbir106706@gmail.com>
2020-06-18 00:31:13 +0600

    abc010-A

---
+++ AtCoder Beginner Contest 010/A-Handle name.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    string str,k;
+    cin>>str;
+    k="pp";
+    cout<<str+k<<"\n";
+    return 0;
+}
+
+
