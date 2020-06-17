02b4a4fe3bc20eaf565c0c33b5f1662b81f1603c
Sabbir <sabbir106706@gmail.com>
2020-06-18 00:22:15 +0600

    abc009-A

---
+++ AtCoder Beginner Contest 009/A-Moving work.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int i;
+    cin>>i;
+    if(i%2==0)
+    {
+        cout<<i/2<<"\n";
+    }
+    else{
+        cout<<(i/2)+1<<"\n";
+    }
+    return 0;
+}
+
+
