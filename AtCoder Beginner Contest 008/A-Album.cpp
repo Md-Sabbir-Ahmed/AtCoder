29a71fbe06cbda914b15d991e53b88103fe7d014
Sabbir <sabbir106706@gmail.com>
2020-06-17 23:57:36 +0600

    abc008-A

---
+++ AtCoder Beginner Contest 008/A-Album.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int s,t,c=0;
+    cin>>s>>t;
+    for(int i=s;i<=t;i++)
+    {
+        c++;
+    }
+    cout<<c<<"\n";
+    return 0;
+}
+
+
