6cf8080b9d2e7e0ea4d0ec3f5a0025b6e6fcebc1
Sabbir <sabbir106706@gmail.com>
2020-06-26 01:37:40 +0600

    abc013-A

---
+++ AtCoder Beginner Contest 013/A - A.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char n;
+    cin>>n;
+    if(n=='A')
+    {
+        cout<<"1\n";
+    }
+    if(n=='B')
+    {
+        cout<<"2\n";
+    }
+    if(n=='C')
+    {
+        cout<<"3\n";
+    }
+    if(n=='D')
+    {
+        cout<<"4\n";
+    }
+    if(n=='E')
+    {
+        cout<<"5\n";
+    }
+    return 0;
+}
+
+
