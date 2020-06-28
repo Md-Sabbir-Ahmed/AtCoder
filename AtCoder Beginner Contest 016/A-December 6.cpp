20efb111ae892e4eb7cfb9082c9b24d6bb731873
Sabbir <sabbir106706@gmail.com>
2020-06-29 01:23:00 +0600

    abc016-A

---
+++ AtCoder Beginner Contest 016/A-December 6.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int m,d;
+    cin>>m>>d;
+    if(m%d==0)
+    {
+        cout<<"YES\n";
+    }
+    else{
+        cout<<"NO\n";
+    }
+
+    return 0;
+}
+
+
