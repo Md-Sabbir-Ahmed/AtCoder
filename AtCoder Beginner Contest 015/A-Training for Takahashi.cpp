7946e7acea971869adf61491e0149459762995ee
Sabbir <sabbir106706@gmail.com>
2020-07-05 00:57:06 +0600

    abc015-A

---
+++ AtCoder Beginner Contest 015/A-Training for Takahashi.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string a,b;
+    cin>>a>>b;
+    int i,j;
+    i=a.size();
+    j=b.size();
+    if(i>j)
+    {
+        cout<<a<<"\n";
+    }
+    else{
+        cout<<b<<"\n";
+    }
+    return 0;
+}
+
+
