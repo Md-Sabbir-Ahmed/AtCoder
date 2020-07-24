3b096018b30bd3b08f510ac6c42f82723729c078
Sabbir <sabbir106706@gmail.com>
2020-07-24 21:48:26 +0600

    jekono messege

---
+++ balobasha/two coins.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a+b>=c)
+    {
+    	cout<<"Yes\n";
+    }
+    else
+    {
+    	cout<<"No\n";
+    }
+    
+    return 0;
+}
+
